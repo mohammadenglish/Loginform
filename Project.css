5dcdef0506951870dd764e3477ccc6453e685ed6
mohammadenglish <minerbig07@gmail.com>
2022-02-02 14:45:12 +0330

    loginform

---
+++ Project.css
@@ -0,0 +1,346 @@
+body{
+	direction: rtl;
+	opacity: 1;
+}
+.container{
+	direction: rtl;
+	font-size: 20px;
+	font-weight:900;
+	font-family: b nazanin;
+	margin: 20px auto;
+	text-align: center;
+	max-width: 100%;
+}
+ul{
+	list-style: none;
+	display: inline-block;
+}
+.main li{
+	float: right;
+	margin: 10px;
+	border: 2px solid white;
+	border-radius: 6px;
+	padding: 3px;
+	cursor: pointer;
+	background-color: white;
+	color: black;
+	position: relative;
+	transition: 0.5s;
+}
+.main li:hover{
+	background-color: #78909C;
+	color: #E0F7FA;
+}
+li ul{
+	position: absolute;
+	top: 100%;
+	left: -22%;
+	width: 142px;
+	z-index: 1;
+	opacity: 0;
+	display: ;
+	transition: 0.5s;
+}
+li ul li{
+	width: 100%;
+}
+li:hover ul{
+	opacity: 1;
+	display: block;
+}
+li.sub::after{
+	content: "\25bc";
+	font-size: 11px;
+	
+}
+li.sub:hover::after{
+	content:"\25b2";
+	font-size:11px;
+}
+.points{
+	font-family: b nazanin;
+	font-size: 20px;
+}
+.point1{
+	margin-top: 5px;
+	background-color: #4CAF50;
+	padding: 15px;
+	width: 96%;
+	border-radius: 30px;
+	text-align: center;
+	color: white;
+}
+.point2{
+	margin-top: 5px;
+	background-color: #4CAF50;
+	padding: 15px;
+	width: 96%;
+	border-radius: 30px;
+	text-align: center;
+	color: white;
+}
+.loginpaperheader{
+	font-size: xxx-large;
+	font-family: b nazanin;
+	text-align: center;
+	font-weight: 900;
+	cursor: pointer;
+}
+.info{
+	font-size: 25px;
+	font-family: b nazanin;
+	font-weight: 600;
+	cursor: pointer;
+	min-height: 6;
+}
+.buttons{
+	font-family: b nazanin;
+	font-size: 20px;
+}
+.inputbutton1{
+	font-size: 30px;
+	font-family: b nazanin;
+	margin-top: 20px;
+	margin-left: 10px;
+	border-radius: 10px;
+	border: 2px solid grey;
+	cursor: pointer;
+	width: 100px;
+	opacity: 0.5;
+	transition: 0.3s;
+	font-weight: 600;
+}
+.inputbutton1:hover{
+	opacity: 1;
+	color: green;
+	border-color: green;
+	background-color: white;
+}
+.inputbutton2{
+	font-size: 30px;
+	font-family: b nazanin;
+	margin-top: 20px;
+	margin-left: 10px;
+	border-radius: 10px;
+	border: 2px solid grey;
+	cursor: pointer;
+	width: 150px;
+	opacity: 0.5;
+	transition: 0.3s;
+	font-weight: 600;
+}
+.inputbutton2:hover{
+	opacity: 1;
+	color: red;
+	background-color: white;
+	border-color: red;
+}
+.container{
+	display: block;
+}
+figcaption {
+	color: black;
+	font-size: 20px;
+	text-align: center;
+}
+a{
+	text-decoration: none;
+	font-weight: bold;
+	color: black;
+}
+figure.orderbook{
+	border: 3px solid black;
+	width: 210px;
+	height: 210px;
+	cursor: pointer;
+	float: right;
+	border-radius: 30px;
+}
+figure img{
+	border-radius: 20px;
+	margin-bottom: 5px;
+	padding: 5px;
+	width: 150px;
+	height: 150px;
+	
+}
+figure:hover img{
+	transform: scale(1.01);
+	overflow: hidden;
+	background-color: red;
+	border-radius: 30px;
+}
+figure:hover a{
+	color: green;
+}
+ul.bookmenu{
+	float: right;
+	background-color: pink;
+	padding: 10px;
+	border-radius: 20px;
+	width: 96%;
+	text-align: center;
+	overflow: hidden;
+	margin: 1px 1px 1px 99px;
+}
+ul.bookmenu li{
+	float: right;
+	text-decoration: none;
+	list-style-type: none;
+	overflow: hidden;
+}
+ul li a{
+	text-decoration: none;
+	color: black;
+	padding: 20px;
+	list-style-type: none;
+	float: left;
+	margin: 3px;
+	cursor: pointer;
+	font-size: 20px;
+	transition: 0.9s;
+	border-radius: 20px;
+}
+ul li a:hover {
+	background-color: white;
+	color: green;
+}
+h3.ads{
+	font-family: b nazanin;
+	margin: 20px 50%;
+	float: right;
+	background-color: #FFEB3B;
+	padding: 50px;
+	border-radius: 30px;
+	color: black;
+}
+.telegram{
+	float:right;
+	background-color: #AED581;
+	border-radius: 20px;
+	padding: 20px;
+	width: 96%;
+	margin-bottom: 10px;
+}
+span.telegram{
+	font-size: 20px;
+	font-weight: 600;
+}
+span a{
+	text-decoration: underline;
+	font-weight: 100;
+	margin-right: 7px;
+	transition: 0.9s;
+}
+.first:hover{
+	color: red;
+}
+.insta{
+	float:right;
+	background-color: #FF7043;
+	border-radius: 20px;
+	padding: 20px;
+	width: 96%;
+	margin-bottom: 10px;	
+}
+span.insta{
+	font-size: 20px;
+	font-weight: 600;
+}
+.second:hover{
+	color: white;
+}
+.site{
+	float:right;
+	background-color: #64B5F6;
+	border-radius: 20px;
+	padding: 20px;
+	width: 96%;
+	margin-bottom: 10px;	
+}
+span.site{
+	font-size: 20px;
+	font-weight: 600;
+}
+.third:hover{
+	color: #FF3D00;
+}
+.personal{
+	float:right;
+	background-color: #546E7A;
+	border-radius: 20px;
+	padding: 20px;
+	width: 96%;
+	margin-bottom: 10px;		
+}
+span.personal{
+	font-size: 20px;
+	font-weight: 600;
+}
+.fourth:hover{
+	color:#FAFAFA;
+}
+h4{
+	float: right;
+	margin: 20px 40%;
+	padding: 20px;
+	font-family: b nazanin;
+	font-size: xxx-large;
+	color: green;
+}
+.gallery{
+	margin: 10px 0px;
+	cursor: pointer;
+}
+.gallery article{
+	width: 300px;
+	height: 200px;
+	float: right;
+	margin: 0 15px 0 15px;
+	padding: 5px;
+	position: relative;
+}
+.images{
+	width: 300px;
+	height: 200px;
+	padding: 0;
+	margin: 0;
+	background-color: white;
+	overflow: hidden;
+	border-radius: 30px;
+}
+img.book1{
+	transition: 0.5s;
+	opacity: 0.8;
+}
+img.book1:hover{
+	opacity: 1;
+}
+img.book2{
+	transition: 0.5s;
+	opacity: 0.8;
+}
+img.book2:hover{
+	opacity: 1;
+}
+img.book3{
+	transition: 0.5s;
+	opacity: 0.8;
+}
+img.book3:hover{
+	opacity: 1;
+}
+img.book4{
+	transition: 0.5s;
+	opacity: 0.8;
+}
+img.book4:hover{
+	opacity: 1;
+}
+img.book5{
+	transition: 0.5s;
+	opacity: 0.8;
+}
+img.book5:hover{
+	opacity: 1;
+}
---
+++ Project.html
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>
+Mohammad Web
+</title>
+<meta charset="utf-8">
+<link rel="stylesheet" type="text/css" href="Project.css">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+</head>
+<body>
+<!--Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod 
+tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud 
+exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel 
+eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat 
+nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril 
+delenit augue duis dolore te feugait nulla facilisi.Javad/images.jpg / Javad/download2.png /
+Javad/triangle.png -->
+<div class="container">
+<ul class="main">
+<li>خانه</li>
+<li class="sub">دیگر محصولات
+<ul class="sub">
+<li>کتاب های تاریخی</li>
+<li>رمان</li>
+<li>آفزایش درآمد</li>
+<li>کتاب های سیاسی</li>
+</ul>
+</li>
+<li>انتقادات و پیشنهادات</li>
+<li class="sub">ارتباط باما
+<ul class="sub">
+<li>ایمیل</li>
+<li>مراجعه حضوری</li>
+<li>تماس</li>
+</ul>
+</li>
+<li>درباره ما</li>
+</ul>
+</div>
+<br><br><br><br><br><br><br><br><br><br>
+<fieldset>
+<legend class="loginpaperheader">فرم ثبت نام</legend>
+<form>
+<div class="points">
+<p class="point1"><b>قسمت های دارای * باید پرشوند</p>
+<p class="point2">اطلاعات خود را با دقت وارد کنید</b>.</p>
+</div>
+<section class="info">
+<label for="username">* نام و نام خانوادگی</label> <input type="text" id="username" name="Username" placeholder="اکبر احمدی" required><br>
+<label for="password">* رمز ورود</label> <input type="password" id="password" name="Password" placeholder="123456" required><br>
+<label for="email">* ایمیل</label> <input type="email" id="email" name="Email" placeholder="akbari@email.com" required>
+<section class="buttons"><input type="submit" value="ثبت" class="inputbutton1"> <input type="reset" value="پاک کن" class="inputbutton2"></section>
+</form>
+</section>
+</fieldset>
+<div class="container">
+<figure class="orderbook">
+<a href="https://b2n.ir/minibook3" target="_balnk"><img src="Javad/book3.jpg" alt="book3">
+<figcaption>
+ باشگاه پنج صبحی ها</a>
+<figcaption>
+</figure>
+<figure class="orderbook">
+<img src="New folder/book1.jpg" alt="book1">
+<figcaption>
+<a href="https://b2n.ir/minibook3" target="_balnk">اثر مرکب</a>
+<figcaption>
+</figure>
+<figure class="orderbook">
+<img src="New folder/book4.jpg" alt="book">
+<figcaption>
+<a href="https://b2n.ir/minibook3" target="_balnk">بی اندیشید و ثروتمند شوید</a>
+<figcaption>
+</figure>
+</div>
+<ul class="bookmenu">
+<li><a href="#"><i class="fas fa-shopping-cart"></i> خریدن</a></li>
+<li><a href="#"><i class="fas fa-info"></i> اطلاعات بیشتر</a></li>
+<li><a href="#"><i class="fas fa-user-edit"></i> دیگر نویسندگان</a></li>
+</ul>
+<h3 class="ads">تبلیغات</h3>
+<div class="telegram">
+<span class="telegram">برای اطلاع از تخفیفات لحظه ایی عوض کانال تگرام ماشوید<a href="#" target="_balnk" class="first">لینک کانال تگرام...</a></span>
+</div>
+<div class="insta">
+<span class="insta">پیج اینستاگرام مارا دنبال کنید تا تخفیف هارا از دست ندهید <a href="#" target="_balnk" class="second">مشاهده پیج...</a></span>
+</div>
+<div class="site">
+<span class="site">آموزش های مختلف توسط اساتید مجرب و برجسته در سایت، از دست ندهید<a href="#" target="_balnk" class="third">بازدید از سایت...</a></span>
+</div>
+<div class="personal">
+<span class="personal">دوره های محمد ابراهیمی در سایت بروزرسانی شد از دست ندهید<a href="#" target="_balnk" class="fourth">مشاهده دوره های محمد ابراهیمی...</a></span>
+</div>
+</div>
+<h4><q> کتابخانه </q></h4>
+<div class="gallery">
+<article>
+<figure class="images">
+<img src="New folder/book1.jpg" alt="book1" class="book1">
+</figure>
+</article>
+<article>
+<figure class="images">
+<img src="New folder/book2.jpg" alt="book2" class="book2">
+</figure>
+</article>
+<article>
+<figure class="images">
+<img src="New folder/book3.jpg" alt="book3" class="book3">
+</figure>
+</article>
+<article>
+<figure class="images">
+<img src="New folder/book4.jpg" alt="book4" class="book4">
+</figure>
+</article>
+<article>
+<figure class="images">
+<img src="New folder/book5.jpg" alt="book5" class="book5">
+</figure>
+</article>
+</div>
+</body>
+</html> 
